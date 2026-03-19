a46d066525f837e011ea16f4f818ffadb0c79d61
kferrano <rzimpel6@gmail.com>
2026-03-19 18:56:50 +0100

    Migrate to NeoForge

--- src/main/java/com/hardrock/nocobbottle/NoCobbleBottleMod.java
+++ src/main/java/com/hardrock/nocobbottle/NoCobbleBottleMod.java
@@ -4,25 +4,24 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
-
 package com.hardrock.nocobbottle;
 
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TickEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
 
 @Mod("no_cobble_bottle")
 public class NoCobbleBottleMod {
-    public NoCobbleBottleMod() {
-        MinecraftForge.EVENT_BUS.register(this);
-    }
-
     private final Tracker tracker = new Tracker();
 
+    public NoCobbleBottleMod() {
+        NeoForge.EVENT_BUS.register(this);
+    }
+
     @SubscribeEvent
-    public void onPlayerTick(TickEvent.PlayerTickEvent e) {
-        if (e.phase != TickEvent.Phase.END || e.player.level().isClientSide) return;
-        tracker.tick(e.player);
+    public void onPlayerTick(PlayerTickEvent.Post e) {
+        if (e.getEntity().level().isClientSide()) return;
+        tracker.tick(e.getEntity());
     }
 }
